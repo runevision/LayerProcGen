b10644be2e0f06c122bbeb2792bfc7a4598a4853
Oli Scherer <github@oli-obk.de>
2024-11-10 11:33:36 +0100

    Actually test the level passed to IsLoadedAtPosition

--- Src/LayerProcGen/ChunkBasedDataLayer.cs
+++ Src/LayerProcGen/ChunkBasedDataLayer.cs
@@ -132,9 +132,9 @@ namespace Runevision.LayerProcGen {
 		/// <param name="index">An integer coordinate that indexes into the grid of chunks.</param>
 		/// <param name="chunk">The requested chunk, or null.</param>
 		/// <returns>True if the chunk exists and has been generated to at least level 0.</returns>
-		protected bool TryGetChunk(Point index, out C chunk) {
+		protected bool TryGetChunk(Point index, out C chunk, int level = 0) {
 			chunk = chunks[index];
-			return (chunk != null && chunk.level >= 0);
+			return (chunk != null && chunk.level >= level);
 		}
 
 		void CreateAndRegisterChunk(Point index, int level) {
@@ -507,7 +507,7 @@ namespace Runevision.LayerProcGen {
 		/// </summary>
 		public bool IsLoadedAtPosition(DPoint position, int level) {
 			var chunkIndex = new Point(Crd.Div((int)position.x, chunkW), Crd.Div((int)position.y, chunkH));
-			return TryGetChunk(chunkIndex, out C _);
+			return TryGetChunk(chunkIndex, out C _, level);
 		}
 	}
 
